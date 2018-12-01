6aa49e0a1d1cafce923c8781587d91254312be4c
gkslsahin <gkslshn26@gmail.com>
2018-12-02 01:13:15 +0300

    updated models

--- db.js
+++ db.js
@@ -1,3 +1,16 @@
 var mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/userlist",{useMongoClient : true});
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name : String,
+    lastname : String,
+    birth : String,
+    email : String
+});
+
+mongoose.model("User",userSchema);
+
+module.exports = mongoose ;
